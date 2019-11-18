4c4a35f3d1591fed8a5071c24aef7d933fff0591
jameslindsaycs <j.lindsay1@nuigalway.ie>
2019-11-18 13:04:05 +0000

    added testing

--- test/models/athlete_test.rb
+++ test/models/athlete_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class AthleteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "athlete attributes must not be empty" do
+    athlete = Athlete.new
+    assert athlete.invalid?
+    assert athlete.errors[:name].any?
+    assert athlete.errors[:description].any?
+    assert athlete.errors[:rating].any?
+    assert athlete.errors[:image_url].any?
+  end
+
+  test "product rating must be positive" do
+    athlete = athletes(:one)
+  
+    athlete.rating = -1
+    assert athlete.invalid?
+    assert athlete.errors[:rating].any?
+  
+    athlete.rating = 0
+    assert athlete.invalid?
+    assert athlete.errors[:rating].any?
+  
+    athlete.rating = 1
+    assert athlete.valid?
+    assert athlete.errors[:rating].none?
+  end
+
+
+
 end
